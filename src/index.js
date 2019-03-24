469d69363f6f8e5147601fc3bd54d06c1278350f
verymadmax <madmax.verymad@gmail.com>
2019-03-24 18:54:08 +0300

    feat: new solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var bases = [];
+  var zerosMin = Infinity;
+  for (j = 2; j < base;){
+    if (base % j == 0){
+      bases.push(j);
+      base /= j;
+    } else {
+      j++;
+    }
+  }
+  
+  bases.push(base);
+  do {
+    var l = bases.length;
+    newBase = bases[l - 1];
+    var counter = 0;
+    for(i = 0; i < l; i++){
+     if(bases[i] == newBase){
+       counter++;
+       bases.splice(i,1);
+       l--;
+       i--;
+     }  
+    }
+    var buffNumber = number;
+    var zeros=0;
+      while (Math.floor(buffNumber) > 0){
+        buffNumber /= newBase;
+        zeros += Math.floor(buffNumber);
+      }
+      zeros /= counter;
+        if (zeros < zerosMin){
+          zerosMin = Math.floor(zeros);
+        }
+  }
+  while(l > 0);
+  return zerosMin;
 }
